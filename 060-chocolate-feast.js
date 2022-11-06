dbbf78eb51d1a615107e3e25b9a4cfd29c374174
Matthew Sanner <gravityskunk@gmail.com>
2022-11-06 10:50:07 -0600

    initial commit

---
+++ 060-chocolate-feast.js
@@ -0,0 +1,11 @@
+//Determines how many chocolate bars one can eat given and initial amount of money, n, and a cost of bars, c. Then a special promotion is available such that for every m wrappers you return you can get another chocolate bar.
+
+function chocolateFeast(n, c, m) {
+    let barsEaten = Math.floor(n / c);
+    let wrappers = barsEaten;
+    while (wrappers >= m) {
+        barsEaten += Math.floor(wrappers / m);
+        wrappers = wrappers % m + Math.floor(wrappers / m);
+    }
+    return barsEaten;
+}
