16633fa4ccb86f5ae15131390a0ef00e7470a09e
Matthew Sanner <gravityskunk@gmail.com>
2022-11-16 19:12:19 -0600

    added cute solution

--- 063-flatland-space-stations.js
+++ 063-flatland-space-stations.js
@@ -1,14 +1,25 @@
 // Given a number, n, of cities in a straight line, and an array, c, of the locations of cities with space stations- determines the the maximum distance from any city to the nearest space station.
 
 function flatlandSpaceStations(n, c) {
-    c.sort((a, b) => b - a);
+    c.sort((a, b) => a - b);
     let maxBnStations = 0;
-    for (let i = 0; i < c.length - 1; i++) {
-        let dist = c[i] - c[i + 1];
+    for (let s = 0; s < c.length - 1; s++) {
+        let dist = Math.abs(c[s] - c[s + 1]);
         if (dist > maxBnStations) maxBnStations = dist;
     }
     let maxDistFromStat = Math.floor(maxBnStations / 2);
-    if (c[c.length - 1] > maxDistFromStat) maxDistFromStat = c[c.length - 1];
-    if ((n - 1) - c[0] > maxDistFromStat) maxDistFromStat = (n - 1) - c[0];
+    //cities at beginning
+    if (c[0] - 0 > maxDistFromStat) maxDistFromStat = c[0];
+    //cities at end
+    if ((n - 1) - c[c.length - 1] > maxDistFromStat) maxDistFromStat = (n - 1) - c[c.length - 1];
     return maxDistFromStat;
 }
+
+// cute n' easy solution
+// function flatlandSpaceStations(n, c) {
+//     let max = [];
+//     for (let city = 0; city < n; city++) {
+//         max.push(Math.min(...(c.map(citySpace => Math.abs(city - citySpace)))))
+//     }
+//     return Math.max(...max)
+// }
