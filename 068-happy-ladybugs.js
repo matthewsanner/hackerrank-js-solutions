571021f5e2a35b121a91cc50922c7ba4b62000ac
Matthew Sanner <gravityskunk@gmail.com>
2022-11-30 18:27:14 -0600

    made if statement easier to read

--- 068-happy-ladybugs.js
+++ 068-happy-ladybugs.js
@@ -3,9 +3,9 @@
 function happyLadybugs(b) {
     for (let i = 0; i < b.length; i++) {
         // returns NO if there is only 1 of any ladybug color
-        if (b[i] !== "_" && b.indexOf(b[i]) === b.lastIndexOf(b[i])) return "NO"
+        if (b[i] !== "_" && b.indexOf(b[i]) === b.lastIndexOf(b[i])) return "NO";
         // returns NO if there is any unhappy ladybug and no empty spaces
-        if (b.indexOf("_") === -1 && !(b[i] === b[i - 1] || b[i] === b[i + 1])) return "NO"
+        if (b.indexOf("_") === -1 && b[i] !== b[i - 1] && b[i] !== b[i + 1]) return "NO";
     }
-    return "YES"
+    return "YES";
 }
